67400252fe3c61c157e47983da02037549d44e93
kierra <51520037+kierrakay@users.noreply.github.com>
2020-01-23 17:33:01 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get "/posts/:id", to: "posts#show", as: "post"
   get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
   patch "/posts/:id", to: "posts#update"
+
 end
